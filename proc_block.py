55f23da260b1706d5f98106d6077d6cb656d7469
Roman <cvqsrsxl@gmail.com>
2024-10-24 18:32:50 +0300

    10242024

--- proc_block.py
+++ proc_block.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget, QMainWindow, QHBoxLayout, QApplication, QLineEdit, QPushButton, QVBoxLayout, QFileDialog, QLabel
+from PyQt6.QtWidgets import QWidget, QMainWindow, QHBoxLayout, QApplication, QLineEdit, QPushButton, QVBoxLayout, QFileDialog, QLabel, QListWidget
 
 import sys, json
 
@@ -7,19 +7,19 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        #with open("procs.json") as f:
-        #    procs = json.load(f)
+        with open("current_user.json") as f:
+            self.current_user = json.load(f)
+
+        self.setWindowTitle('Face Gate')
 
         # LINE LAYOUT WIDGETS
         layout_line = QHBoxLayout()
 
-        self.file_name = ""
-
-        self.proc_name_line = QLabel("File name: " + self.file_name)
+        self.proc_name_line = QLabel("None selected")
         layout_line.addWidget(self.proc_name_line)
         
         proc_location = QPushButton("Open file")
-        proc_location.clicked.connect(lambda: self.get_file_name(self.proc_name_line))
+        proc_location.clicked.connect(lambda: self.get_file_name())
         layout_line.addWidget(proc_location)
 
         add_proc = QPushButton("Add")
@@ -28,6 +28,15 @@ class MainWindow(QMainWindow):
 
         # PROCS LAYOUT WIDGETS
         layout_procs = QVBoxLayout()
+
+        self.procs_list_widget = QListWidget()
+        self.fill_list()
+
+        layout_procs.addWidget(self.procs_list_widget)
+
+        self.del_proc_button = QPushButton("Delete")
+        self.del_proc_button.clicked.connect(self.delete_procs)
+        layout_procs.addWidget(self.del_proc_button)
 
         # COMBINING LAYOUTS
         main_layout = QVBoxLayout()
@@ -49,15 +58,49 @@ class MainWindow(QMainWindow):
     def add_procs(self, proc):
         with open("procs.json", "r") as f:
             procs = json.load(f)
-        procs.update(proc)
+        if self.proc_name_line.text() not in procs[self.current_user] and self.proc_name_line.text() != None and self.proc_name_line.text() != "None selected":
+            procs[self.current_user].append(self.proc_name_line.text())
+            self.procs_list_widget.addItem(self.proc_name_line.text())
+            self.proc_name_line.setText("None selected")
+        else:
+            print("Такой процесс уже есть в списке!")
 
-        json.dump(procs)
-        
-    def get_file_name(self, label):
+        with open("procs.json", "w") as f:
+            json.dump(procs, f)
+
+    def delete_procs(self):
+        with open("procs.json", "r") as f:
+            procs = json.load(f)
+        list_items = self.procs_list_widget.selectedItems()
+        if not list_items:
+            return
+        for item in list_items:
+            self.procs_list_widget.takeItem(self.procs_list_widget.row(item))
+            procs[self.current_user].remove(item.text())
+            with open("procs.json", "w") as f:
+                json.dump(procs, f)
+
+    def get_file_name(self):
         filename, filetype = QFileDialog.getOpenFileName(self,
                              "Choose file",
                              ".",
                              "Executable Files(*.exe)")
-        print(filename)
-        self.file_name = filename.split('/')[-1]
-        self.update()
+        if filename != "" and filetype != "":
+            self.proc_name_line.setText(filename.split('/')[-1])
+
+    def fill_list(self):
+        with open("procs.json", "r") as f:
+            procs = json.load(f)
+
+        for k, v in procs.items():
+            if k == self.current_user:
+                self.procs_list_widget.addItems(procs[k])
+
+    
+            
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
