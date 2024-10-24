aef6297bc54e3ad1ca5f1189a267bd26b18b7f18
Roman <cvqsrsxl@gmail.com>
2024-10-24 18:40:48 +0300

    Delete loginlayout.py

--- loginlayout.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QCheckBox, QMainWindow
-from PyQt6.QtGui import QIcon, QFont
-
-class Login():
-    def __init__(self):
-        self.layout()
-        
-    
